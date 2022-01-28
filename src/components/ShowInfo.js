c6c248be1bc3ada51f3bbe728a11a5792d155818
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-28 08:45:46 +0100

    make both buttons work

--- src/components/ShowInfo.js
+++ src/components/ShowInfo.js
@@ -2,11 +2,16 @@ import { useState } from 'react';
 
 export const ShowInfo = ({ title, children }) => {
 	const [status, setStatus] = useState('notApproved');
+
+	const handleChangeStatus = () => {
+		setStatus(status === 'approved' ? 'notApproved' : 'approved'); 
+	}
+
 	return (
 		<div className="infoBox">
 			<div className="title">{title}</div>
 			<p>This is the info from the function component.</p>
-			<div className="status">STATUS: <span>{status}</span></div>
+			<div className="status"><button onClick={handleChangeStatus}>CHANGE STATUS</button> <span>{status}</span></div>
 			{children}
 		</div>
 	)
--- src/components/ShowInfoAsClass.js
+++ src/components/ShowInfoAsClass.js
@@ -7,12 +7,15 @@ export class ShowInfoAsClass extends Component {
 			status: 'notApproved'
 		};
 	}
+	handleChangeStatus = () => {
+		this.setState({status: this.state.status === 'approved' ? 'notApproved' : 'approved'});
+	}
 	render() {
 		return (
 			<div className="infoBox">
 				<div className="title">{this.props.title}</div>
 				<p>This is the info from the <strong>class</strong> component.</p>
-				<div className="status">STATUS: <span>{this.state.status}</span></div>
+			<div className="status"><button onClick={this.handleChangeStatus}>CHANGE STATUS</button> <span>{this.state.status}</span></div>
 				{this.props.children}
 			</div>
 		)
